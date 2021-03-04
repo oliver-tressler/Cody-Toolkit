b9d5516baffd2a49251a8a667b87dfd1df03c7cb
Oliver Tressler <Oliver.Tressler@ximea.com>
2021-03-04 19:01:29 +0100

    init buildandpublish

---
+++ cody.frontend/cody.buildandpublish/src/extension.ts
@@ -0,0 +1,8 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
