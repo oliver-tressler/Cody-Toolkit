8ec5b1b26092fbb0b456f7212f1cbbfedbd5f8cd
Oliver Tressler <Oliver.Tressler@ximea.com>
2021-03-15 13:25:25 +0100

    restart ts server if required

--- cody.frontend/cody.proxygenerator/src/tsProxyGenerator.ts
+++ cody.frontend/cody.proxygenerator/src/tsProxyGenerator.ts
@@ -1,4 +1,5 @@
 import { normalize } from "path";
+import { existsSync } from "fs";
 import * as vscode from "vscode";
 import {
 	generateEntityProxies as apiGenerateEntityProxies,
@@ -7,8 +8,9 @@ import {
 	retrieveAvailableEntities,
 } from "./Api/api";
 import { Configuration, TypeScriptConfiguration } from "./Configuration/ConfigurationProxy";
-import { ConnectionState, getConnectionState } from "./connection";
+import { ConnectionState, getConnectionState } from "./Utils/connection";
 import { AvailableAction, AvailableEntity, GenerateProxyCommandProvider } from "./sharedTypings";
+import { isSubDirOrEqualDir } from "./Utils/fsUtils";
 
 /**
  * Make sure that the user is logged in to an organization.
@@ -40,6 +42,31 @@ async function requestTsProxyFolderLocation() {
 		throw new Error("A typescript proxy folder needs to be specified in the Configuration");
 	}
 	return normalize(proxyFolder[0].fsPath);
+}
+
+/**
+ * Somehow the typescript language server has issues with new files being created when paths are used. This restarts the
+ * typescript server if the proxy file would be newly created, if the command is available and one of the open workspace
+ * folders is a parent dir of the configured proxy folder.
+ */
+async function withTsServerRestartIfRequired<T>(delegate: Promise<T>): Promise<T | undefined> {
+	if (
+		vscode.workspace.workspaceFolders?.some((wf) =>
+			isSubDirOrEqualDir(wf.uri.fsPath, TypeScriptConfiguration.proxyFolder)
+		) === true &&
+		(await vscode.commands.getCommands(true)).includes("typescript.restartTsServer")
+	) {
+		let delegateResult = await delegate;
+		try {
+			await vscode.commands.executeCommand("typescript.restartTsServer");
+		} catch {
+			// Fail silently for this one. This does not need to bother the user.
+		} finally {
+			return delegateResult;
+		}
+	} else {
+		return await delegate;
+	}
 }
 
 /**
@@ -120,7 +147,7 @@ async function chooseEntities(connectionState: AuthorizedConnectionState) {
 		: chooseEntitiesViaFreeText(connectionState);
 }
 
-function startEntityProxyGeneration({
+async function startEntityProxyGeneration({
 	connectionState,
 	entities,
 }: {
@@ -130,13 +157,15 @@ function startEntityProxyGeneration({
 	if (entities != null && entities.length === 0) {
 		return;
 	}
-	return apiGenerateEntityProxies({
-		entitiyLogicalNames: entities ?? [],
-		language: "ts",
-		organization: connectionState.activeOrganization.UniqueName,
-		path: TypeScriptConfiguration.proxyFolder,
-		globalEnums: TypeScriptConfiguration.globalEnums === true,
-	});
+	return withTsServerRestartIfRequired(
+		apiGenerateEntityProxies({
+			entitiyLogicalNames: entities ?? [],
+			language: "ts",
+			organization: connectionState.activeOrganization.UniqueName,
+			path: TypeScriptConfiguration.proxyFolder,
+			globalEnums: TypeScriptConfiguration.globalEnums === true,
+		})
+	);
 }
 
 /**
@@ -241,12 +270,14 @@ function startActionProxyGeneration({
 	if (actions != null && actions.length === 0) {
 		return;
 	}
-	return apiGenerateActionProxies({
-		actionNames: actions ?? [],
-		language: "ts",
-		organization: connectionState.activeOrganization.UniqueName,
-		path: TypeScriptConfiguration.proxyFolder,
-	});
+	return withTsServerRestartIfRequired(
+		apiGenerateActionProxies({
+			actionNames: actions ?? [],
+			language: "ts",
+			organization: connectionState.activeOrganization.UniqueName,
+			path: TypeScriptConfiguration.proxyFolder,
+		})
+	);
 }
 
 async function generateActionProxies() {
@@ -281,6 +312,6 @@ export const tsCommandProvider: GenerateProxyCommandProvider = {
 		generateEntityProxies,
 		generateActionProxies,
 		regenerateAllEntityProxies,
-		regenerateAllActionProxies
+		regenerateAllActionProxies,
 	},
 };
