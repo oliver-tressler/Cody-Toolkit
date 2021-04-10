62d1116cd8da700816b2e8bdcf2d3a53d09e78dc
Oliver Tressler <oliver.tressler@protonmail.com>
2021-04-10 11:47:44 +0200

    simplify backend dll

--- cody.backend/cody.backend.api/Global.asax.cs
+++ cody.backend/cody.backend.api/Global.asax.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace cody.backend.api
@@ -13,7 +12,6 @@ namespace cody.backend.api
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
